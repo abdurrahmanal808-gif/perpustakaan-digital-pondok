779f95312a08b318e9aa38c379b3dd07a2a81ee2
Amang Habsyi <abdurrahman.al808@sma.belajar.id>
2026-06-10 09:51:08 +0700

    Increase server action body size limit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
-  serverActions: {
-    bodySizeLimit: "2mb"
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb"
+    }
   }
 };
 
